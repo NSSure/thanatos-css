9c4038fe4b17fd43c2e13d5fb857e92f1be1fbf0
NSSure <19178714+NSSure@users.noreply.github.com>
2020-06-03 00:06:58 -0400

    Added new helper data files for documentation

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const path = require("path");
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
 const app = express();
+const scssToJson = require('scss-to-json');
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.set("layout extractScripts", true);
 app.set("layout extractStyles", true);
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'css')));
 app.use(expressLayouts);
 
 const fs = require('fs');
@@ -22,12 +23,12 @@ fs.readdir('./views/docs', (err, files) => {
     app.get(`/${fileName}`, async (req, res) => {
         if (fileName === 'helpers') {
           var sassVariables = {
-            animation: JSON.parse(fs.readFileSync("./data/animation.json")),
-            color: JSON.parse(fs.readFileSync("./data/color.json")),
-            layout: JSON.parse(fs.readFileSync("./data/layout.json")),
-            spacing: JSON.parse(fs.readFileSync("./data/spacing.json")),
-            sizing: JSON.parse(fs.readFileSync("./data/sizing.json")),
-            typography: JSON.parse(fs.readFileSync("./data/typography.json"))
+            animation: JSON.parse(fs.readFileSync("./data/animation-helpers.json")),
+            color: JSON.parse(fs.readFileSync("./data/color-helpers.json")),
+            layout: JSON.parse(fs.readFileSync("./data/layout-helpers.json")),
+            spacing: JSON.parse(fs.readFileSync("./data/spacing-helpers.json")),
+            sizing: JSON.parse(fs.readFileSync("./data/sizing-helpers.json")),
+            typography: JSON.parse(fs.readFileSync("./data/typography-helpers.json"))
           };
 
           res.render(`docs/${fileName}`, { sassVariables: sassVariables });
